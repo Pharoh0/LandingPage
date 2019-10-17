94b304768f7129c8e21191ef4e228e602c6c4e40
Pahroh0 <essameldinart@gmail.com>
2019-10-17 01:42:59 -0700

    change static and media

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,9 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root') #static
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') #static
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root') #media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #media
 MEDIA_URL = '/media/'
-
 
